66fdfeae6560202d13e082a77cd33424fa08d4fb
Sweta Sinha <sinhasweta276@gmail.com>
2026-05-20 12:42:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+st.set_page_config(
+    page_title="ScoreIQ",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 import joblib
 import pandas as pd
 import json, os, hashlib, io, base64, random
